3d1026f2f8bc8498907f10b89e301c672ed93ebb
Thaeyoung Jin <ntyjin@naver.com>
2022-02-26 16:45:55 +0900

    first commit

---
+++ src/or/tyjin/ThaeyoungJin/HelloWorld.java
@@ -0,0 +1,9 @@
+package or.tyjin.ThaeyoungJin;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/org/tyjin/numberstring/charstring.java
@@ -0,0 +1,12 @@
+package org.tyjin.numberstring;
+
+public class charstring {
+
+	public static void main(String[] args) {
+		System.out.println('积');
+		System.out.println("积劝内爹");
+		//System.out.println('积劝内爹');
+		System.out.println("积");
+	}
+
+}
